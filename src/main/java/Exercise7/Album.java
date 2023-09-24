79a13902046165578fd2152b588a355fd8d7f997
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-24 16:23:08 -0300

    feat(Album): constructor implementation

--- src/main/java/Exercise7/Album.java
+++ src/main/java/Exercise7/Album.java
@@ -4,13 +4,23 @@ import Exercise7.Photo;
 
 public class Album {
 
-    private double heigth;
+    private double height;
     private double width;
     private Photo photo1;
     private Photo photo2;
 
 
-
-    public Album() {
+    public Album(double height, double width) {
+        this.height = height;
+        this.width = width;
     }
+
+    public double getHeight() {
+            return height;
+            }
+
+    public double getWidth() {
+            return width;
+        }
+
 }
