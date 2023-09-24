c66141b2e3c0c1e600f153bb469884f315eba9c2
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-24 16:20:32 -0300

    test(Album): create inicial test

--- src/test/java/Exercise7/AlbumTest.java
+++ src/test/java/Exercise7/AlbumTest.java
@@ -1,7 +1,20 @@
 package Exercise7;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class AlbumTest {
+
+    @Test
+    void shouldCreateValidAlbum() {
+        final double height = 6;
+        final double width = 9;
+        final Album sut = new Album(height, width);
+
+        assertEquals(height, sut.getHeight());
+        assertEquals(width, sut.getWidth());
+    }
 
 }
