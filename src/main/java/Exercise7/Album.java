ce598bb1d534710bceed6c858657bfca4a349bfc
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-24 13:35:43 -0300

    feat: Album and photo created

---
+++ src/main/java/Exercise7/Album.java
@@ -0,0 +1,16 @@
+package Exercise7;
+
+import Exercise7.Photo;
+
+public class Album {
+
+    private double heigth;
+    private double width;
+    private Photo photo1;
+    private Photo photo2;
+
+
+
+    public Album() {
+    }
+}
---
+++ src/main/java/Exercise7/Photo.java
@@ -0,0 +1,10 @@
+package Exercise7;
+
+public class Photo {
+
+    private double heigth;
+    private double width;
+
+    public Photo() {
+    }
+}
