75a7bb2d7d55c3a673215fa0c53f7b93844a88f2
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-24 18:03:50 -0300

    feat(Album): rotate photo to fit in the album

--- src/main/java/Exercise7/Album.java
+++ src/main/java/Exercise7/Album.java
@@ -16,6 +16,21 @@ public class Album {
         this.width = width;
     }
 
+    public boolean fitPhotos(){
+        if (!containsPhotos()) return false;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if(fitSideToSide() || fitAboveAndBelow()) return true;
+                photo2.rotate();
+            }
+            photo1.rotate();
+        }
+
+
+        return false;
+    }
+
     public void addPhotos(Photo photo1, Photo photo2){
         if (photo1 == null || photo2 == null) throw new IllegalArgumentException();
         this.photo1 = photo1;
@@ -24,6 +39,14 @@ public class Album {
 
     public boolean containsPhotos() {
         return photo1 != null && photo2 != null;
+    }
+
+    private boolean fitSideToSide() {
+        return photo1.getWidth() + photo2.getWidth() <= width && photo1.getHeight() <= height && photo2.getHeight() <= height;
+    }
+
+    private boolean fitAboveAndBelow() {
+        return photo1.getHeight() + photo2.getHeight() <= height && photo1.getWidth() <= width && photo2.getWidth() <= width;
     }
 
     private void validate(double height, double width){
