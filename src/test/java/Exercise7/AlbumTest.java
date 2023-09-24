0ec1ed7747be1aaea8c02351d1636a196c395448
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-24 17:42:58 -0300

    test(Album): add photosShouldNotFitOnAlbum test case

--- src/test/java/Exercise7/AlbumTest.java
+++ src/test/java/Exercise7/AlbumTest.java
@@ -66,8 +66,26 @@ class AlbumTest {
         final Album sut = new Album(height, width);
         final Photo photo1 = new Photo(photo1Height, photo1width);
         final Photo photo2 = new Photo(photo2Height, photo2width);
+        sut.addPhotos(photo1, photo2);
 
         assertTrue(sut.fitPhotos());
+    }
+
+    @Test
+    void photosShouldNotFitOnAlbum() {
+        final double height = 6;
+        final double width = 9;
+        final double photo1Height = 4;
+        final double photo1width = 6;
+        final double photo2Height = 7;
+        final double photo2width = 4;
+
+        final Album sut = new Album(height, width);
+        final Photo photo1 = new Photo(photo1Height, photo1width);
+        final Photo photo2 = new Photo(photo2Height, photo2width);
+        sut.addPhotos(photo1, photo2);
+
+        assertFalse(sut.fitPhotos());
     }
 
 
