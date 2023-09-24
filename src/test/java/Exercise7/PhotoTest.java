0c4ced09a3c4ca7560517bfdb251c2a1d64b971c
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-24 13:58:00 -0300

    test: add tests for creating valid and invalid Photos

--- src/test/java/Exercise7/PhotoTest.java
+++ src/test/java/Exercise7/PhotoTest.java
@@ -1,7 +1,36 @@
 package Exercise7;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhotoTest {
+
+    @Test
+    void shouldCreateValidPhotoSize() {
+        final double heigth = 3.5;
+        final double width = 5;
+        final Photo sut = new Photo(heigth, width);
+        assertEquals(heigth, sut.getHeigth());
+        assertEquals(width, sut.getWidth());
+    }
+
+    @Test
+    void shouldThrowExceptionForInvalidPhotoSize1() {
+        final double heigth = 0;
+        final double width = 5;
+        final Photo sut = new Photo(heigth, width);
+        assertThrows(IllegalArgumentException.class, IllegalArgumentException::new);
+    }
+
+    @Test
+    void shouldThrowExceptionForInvalidPhotoSize2() {
+        final double heigth = -2;
+        final double width = -3.6;
+        final Photo sut = new Photo(heigth, width);
+        assertThrows(IllegalArgumentException.class, IllegalArgumentException::new);
+    }
+
 
 }
