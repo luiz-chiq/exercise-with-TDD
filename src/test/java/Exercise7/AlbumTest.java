8afba68fb7e7e7dbefa12a5762772f6b7eb3657a
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-24 18:01:04 -0300

    test(Album): add photosShouldRotateBothPhotosToFitOnAlbum test case

--- src/test/java/Exercise7/AlbumTest.java
+++ src/test/java/Exercise7/AlbumTest.java
@@ -139,6 +139,21 @@ class AlbumTest {
         assertTrue(sut.fitPhotos());
     }
 
+    @Test
+    void photosShouldRotateBothPhotosToFitOnAlbum() {
+        final double height = 9;
+        final double width = 6;
+        final double photo1Height = 6;
+        final double photo1width = 4;
+        final double photo2Height = 6;
+        final double photo2width = 4;
 
+        final Album sut = new Album(height, width);
+        final Photo photo1 = new Photo(photo1Height, photo1width);
+        final Photo photo2 = new Photo(photo2Height, photo2width);
+        sut.addPhotos(photo1, photo2);
+
+        assertTrue(sut.fitPhotos());
+    }
 
 }
